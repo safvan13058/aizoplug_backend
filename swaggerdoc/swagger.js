3daacb51b30bd22b773a2e194d9c4288a86c56d9
safvan13058 <safvan13473@gmail.com>
2025-04-19 15:38:55 +0530

    Fix security scheme type from "https" to "http" in Swagger documentation

--- swaggerdoc/swagger.js
+++ swaggerdoc/swagger.js
@@ -16,7 +16,7 @@ const options = {
   components: {
     securitySchemes: {
       bearerAuth: {
-        type: "https",
+        type: "http",
         scheme: "bearer",
         bearerFormat: "JWT",
       },
