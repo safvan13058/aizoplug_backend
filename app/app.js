434692ee3f6ddb25ad0004c224479cb2d7fdd54d
safvan13058 <safvan13473@gmail.com>
2025-04-29 15:49:26 +0530

    change

--- app/app.js
+++ app/app.js
@@ -222,7 +222,7 @@ const {addstations} = require('../dashboard/stations');
 const {getchargingsession,sessionBilling} = require('./chargingsession');
 
 app.get('/api/sessions/recent', validateJwt, authorizeRoles('admin', 'customer', 'staff', 'dealer'), getchargingsession);
-// app.get('/api/session/billing/:session_id',sessionBilling)
+app.get('/api/session/billing/:session_id',sessionBilling)
 
 // ----------------------------------share access------------------------------------------------------
 const {shareaccess}=require('./accessshare')
